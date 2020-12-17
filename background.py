39717f15e62e8e823185820072668d1626231dc1
noanner <75714327+noanner@users.noreply.github.com>
2020-12-17 22:14:24 +0800

    背景圖

---
+++ background.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+# 背景
+canvas = tk.Canvas(root, width = 3000, height = 1200, bd = 0, highlightthickness = 0)
+imgpath = 'tenor-1.gif'
+img = Image.open(imgpath)
+photo = ImageTk.PhotoImage(img)
+
+canvas.create_image(700, 500, image = photo)
+canvas.pack()
+entry = tk.Entry(root, insertbackground = 'blue', highlightthickness = 2)
+entry.pack()
+
+canvas.create_window(100, 50, width = 100, height = 20,
+                     window = entry)
+
+root.mainloop()
