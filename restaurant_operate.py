addd2e42d451701caff004c8e32e20c9b724ba8b
formosachiang <75771247+formosachiang@users.noreply.github.com>
2020-12-21 12:00:29 +0800

    更新 restaurant_operate, result

--- restaurant_operate.py
+++ restaurant_operate.py
@@ -5,20 +5,20 @@ from PIL import ImageTk
 win = tk.Tk()  # 建立主視窗
 
 # 視窗標題
-win.title("Day1")
+win.title("餐廳經營小遊戲")
 
 # 視窗大小
-win.geometry("900x500+200+100")
-win.minsize(width=900, height=500)
+win.geometry("900x600+200+30")
+win.minsize(width=900, height=600)
 #win.maxsize(width=1024, height=768)
 
 # 視窗顏色
 win.config(background="#F3F3F3")
 
 # 顯示Day 1 
-f1 = tkFont.Font(size=32, family="Courier New")
+f1 = tkFont.Font(size=30, family="微軟正黑體")
 header = tk.Label(text="Day 1", bg="#F3F3F3", fg="black", font=f1)
-header.place(x=20, y=20)
+header.place(x=26, y=20)
 
 # 行事曆按鈕
 def show_calender():
@@ -27,24 +27,24 @@ def show_calender():
     """
     pass
 
-calender = tk.Button(text="Calender", width=18, height=4, bd=3)
-calender.config(background="skyblue")  # 第二種方式
-calender.config(command=show_calender)
+f2 = tkFont.Font(size=14, family="微軟正黑體")
+calender = tk.Button(command=show_calender(), text="行事曆", width=7, height=2, font=f2)
+calender.config(background="Lavender")  # 第二種方式
 
-calender.pack(side=tk.TOP, anchor=tk.NE)  # 行事曆封裝
+calender.place(x=780, y=10)  # 行事曆封裝
 
 # 做漢堡的圖片
-image = ImageTk.Image.open("C:\\Users\\formo\\Documents\\python files\\finalproject\\mytest\\S__85836045.png")
-image = image.resize((400, 200), ImageTk.Image.ANTIALIAS)
+image = ImageTk.Image.open("C:\\Users\\formo\\Documents\\python files\\finalproject\\PBC_group92\\S__85836045.png")
+image = image.resize((600, 300), ImageTk.Image.ANTIALIAS)
 cooking_img = ImageTk.PhotoImage(image)
 panel = tk.Label(win, image = cooking_img)
-panel.pack()
+panel.place(x=150, y=100)
 #panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 # 顯示Day 1 
-f3 = tkFont.Font(size=15, family="Courier New")
+f3 = tkFont.Font(size=15, family="微軟正黑體")
 text1 = tk.Label(text="餐廳開始營業囉，點選下一頁查看你今天的營業成果吧~", bg="#F3F3F3", fg="black", font=f3)
-text1.place(x=200, y=350)
+text1.place(x=200, y=450)
 
 # 下一頁按鈕
 def go_next_page():
@@ -53,9 +53,9 @@ def go_next_page():
     """
     pass
 
-next_page = tk.Button(text="下一頁", width=20, height=5, bd=3)
-next_page.config(background="skyblue")  # 第二種方式
-next_page.config(command=show_calender)
+next_page = tk.Button(text="下一頁", width=7, height=2, font=f2)
+next_page.config(background="Lavender")  # 第二種方式
+next_page.config(command=show_calender())
 
 next_page.pack(side=tk.BOTTOM, anchor=tk.SE)  # 下一頁按鈕封裝
 
--- restaurant_result.py
+++ restaurant_result.py
@@ -6,23 +6,23 @@ from tkinter import ttk
 win = tk.Tk()  # 建立主視窗
 
 # 視窗標題
-win.title("Day1")
+win.title("餐廳經營小遊戲")
 
 # 視窗大小
-win.geometry("900x500+200+100")
-win.minsize(width=900, height=500)
+win.geometry("900x600+200+30")
+win.minsize(width=900, height=600)
 #win.maxsize(width=1024, height=768)
 
 # 視窗顏色
 win.config(background="#F3F3F3")
 
 # 顯示 header
-f1 = tkFont.Font(size=32, family="Courier New")
+f1 = tkFont.Font(size=30, family="微軟正黑體")
 header = tk.Label(text="Day 1", bg="#F3F3F3", fg="black", font=f1)  # Day 1
-header.place(x=20, y=20)
+header.place(x=26, y=20)
 
-f1 = tkFont.Font(size=10, family="Courier New")
-lb = tk.Label(text="辛苦了~下面是你今天營業的成果~~!!", bg="#F3F3F3", fg="black", font=f1)  # 辛苦了...
+f2 = tkFont.Font(size=14, family="微軟正黑體")
+lb = tk.Label(text="辛苦了~下面是你今天營業的成果~~!!", bg="#F3F3F3", fg="black", font=f2)  # 辛苦了...
 lb.place(x=200, y=40)
 # lb.place(anchor=tk.CENTER, x=200, y=40) anchor=原點位置
 
@@ -33,24 +33,22 @@ def show_calender():
     """
     pass
 
-calender = tk.Button(text="Calender", width=18, height=4, bd=3)
-calender.config(background="skyblue")  # 第二種方式
-calender.config(command=show_calender)
+calender = tk.Button(command=show_calender(), text="行事曆", width=7, height=2, font=f2)
+calender.config(background="Lavender")  # 第二種方式
 
-calender.pack(side=tk.TOP, anchor=tk.NE)  # 行事曆封裝
-
-# 做漢堡的圖片
-#cooking_img = PhotoImage(file="C:\Users\\formo\\Documents\\python files\\finalproject\\mytest\\S__85836045.png")
+calender.place(x=780, y=10)  # 行事曆封裝
 
 # 品項表格
 tree_item=ttk.Treeview(win)#表格
 tree_item["columns"]=("牛肉漢堡","豬肉堡堡","雞肉漢堡","生菜堡","生酮堡堡")
-tree_item.column("牛肉漢堡",width=70)   #表示列,不顯示
-tree_item.column("豬肉堡堡",width=70)
-tree_item.column("雞肉漢堡",width=70)
-tree_item.column("生菜堡",width=70)
-tree_item.column("生酮堡堡",width=70)
+tree_item.column("#0", minwidth=0, width=110)
+tree_item.column("牛肉漢堡",width=80, anchor="center")   #表示列,不顯示
+tree_item.column("豬肉堡堡",width=80, anchor="center")
+tree_item.column("雞肉漢堡",width=80, anchor="center")
+tree_item.column("生菜堡",width=80, anchor="center")
+tree_item.column("生酮堡堡",width=80, anchor="center")
 
+tree_item.heading("#0", text="品項")
 tree_item.heading("牛肉漢堡",text="牛肉漢堡")  #顯示錶頭
 tree_item.heading("豬肉堡堡",text="豬肉堡堡")
 tree_item.heading("雞肉漢堡",text="雞肉漢堡")
@@ -63,12 +61,18 @@ tree_item.insert("",2,text="賣出數量" ,values=("20","20","20","20","20"))
 tree_item.insert("",3,text="營業額" ,values=("400","360","360","240","640"))
 tree_item.insert("",4,text="營業額百分比" ,values=("20%","18%","18%","12%","32%"))
 
-tree_item.place(x=20, y=140, height=180)
+style = ttk.Style()
+style.configure("Treeview", rowheight=50)
+# style.configure("Treeview.Heading", font=(None, 12))
+
+tree_item.place(x=60, y=140, height=300)
 
 # 金額表格
 tree_amount=ttk.Treeview(win)#表格
 tree_amount["columns"]=("金額")
-tree_amount.column("金額",width=70)   #表示列,不顯示
+tree_amount.column("#0", minwidth=0, width=100)
+tree_amount.column("金額",width=100, anchor="center")   #表示列,不顯示
+tree_amount.heading("#0", text="品項")
 tree_amount.heading("金額",text="金額")  #顯示錶頭
 tree_amount.insert("",0,text="今日收益" ,values=("2000")) #插入資料，
 tree_amount.insert("",1,text="原料成本" ,values=("0"))
@@ -76,7 +80,7 @@ tree_amount.insert("",2,text="進貨成本" ,values=("0"))
 tree_amount.insert("",3,text="存貨成本" ,values=("50"))
 tree_amount.insert("",4,text="本日淨利" ,values=("1950"))
 
-tree_amount.place(x=600, y=140, height=180)
+tree_amount.place(x=620, y=140, height=300)
 
 
 # 下一頁按鈕
@@ -86,9 +90,9 @@ def go_next_page():
     """
     pass
 
-next_page = tk.Button(text="下一頁", width=20, height=5, bd=3)
-next_page.config(background="skyblue")  # 第二種方式
-next_page.config(command=show_calender)
+next_page = tk.Button(text="下一頁", width=7, height=2, font=f2)
+next_page.config(background="Lavender")  # 第二種方式
+next_page.config(command=show_calender())
 
 next_page.pack(side=tk.BOTTOM, anchor=tk.SE)  # 下一頁按鈕封裝
 
