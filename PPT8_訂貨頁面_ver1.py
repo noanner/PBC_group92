058e7eb7d2aa61cbfa8b4173197c820e030f67fb
R08741013 <75712348+R08741013@users.noreply.github.com>
2020-12-21 12:12:35 +0800

    Update PPT8_訂貨頁面_ver1.py
    更新訂貨頁面

--- PPT8_訂貨頁面_ver1.py
+++ PPT8_訂貨頁面_ver1.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 from PIL import ImageTk
 
-class Window(tk.Frame):
+class Ordering(tk.Frame):
 
     def __init__(self):
         tk.Frame.__init__(self) 
@@ -10,14 +10,15 @@ class Window(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        f1 = tkFont.Font(size = 22, family = "微軟正黑體")
+        f1 = tkFont.Font(size = 30, family = "微軟正黑體")
         f2 = tkFont.Font(size = 14, family = "微軟正黑體")
         f3 = tkFont.Font(size = 12, family = "微軟正黑體")
         
         # row0 標題、敘述、行事曆按鈕
-        self.lbl_topic = tk.Label(self, text = "Day1", height = 2, width = 7, font = f1)
+        self.lbl_gridonly = tk.Label(self, text = " ", height = 2, width = 30, font = f1)
+        self.lbl_topic = tk.Label(self, text = "Day1", height = 1, width = 5, font = f1)
         self.lbl_description = tk.Label(self, text = "以下是目前的剩餘庫存，請問今天要訂多少貨呢?", height = 2, width = 40, font = f3)
-        self.btn_schedule = tk.Button(self, text = "行事曆", command = self.clickBtnSchedule, height = 2, width = 6, font = f2, bg = 'Lavender')
+        self.btn_schedule = tk.Button(self, text = "行事曆", command = self.clickBtnSchedule, height = 2, width = 7, font = f2, bg = 'Lavender')
         
         # row1 訂購數量表格的標題欄
         self.lbl_r1c1 = tk.Label(self, text = "食材", height = 2, width = 8, font = f3, bg = 'LemonChiffon')
@@ -57,21 +58,30 @@ class Window(tk.Frame):
         
         # row7 訂貨固定成本、目前訂購總價、訂購按鈕
         self.lbl_fixcost = tk.Label(self, text = " 訂貨固定成本:$50元 ", height = 2, width = 15, font = f3, bg = 'LemonChiffon')
-        self.lbl_cost = tk.Label(self, text = "總價:$$$$$", height = 1, width = 7, font = f3, bg = 'OldLace')
-        self.btn_order = tk.Button(self, text = "訂購!", command = self.clickBtnOrder, height = 1, width = 5, font = f2, bg = 'Lavender')
+        self.lbl_cost = tk.Label(self, text = "總價:$$$$$", height = 2, width = 7, font = f3, bg = 'OldLace')
+        self.btn_order = tk.Button(self, text = "訂購!", command = self.clickBtnOrder, height = 2, width = 7, font = f2, bg = 'Lavender')
         
         #-----------------------------------------------------------------------------------------------
         # row0
-        self.lbl_topic.grid(row = 0, column = 0, columnspan = 2, sticky = tk.W)
-        self.lbl_description.grid(row = 0, column = 2, columnspan = 4, sticky = tk.W)
-        self.btn_schedule.grid(row = 0, column = 6, padx = 10, pady = 10)
+        self.lbl_gridonly.grid(row = 0, column = 0, columnspan = 10, sticky = tk.NW)
+        # self.lbl_topic.grid(row = 0, column = 0, sticky = tk.NW)
+        # self.lbl_description.grid(row = 0, column = 1, sticky = tk.NW)
+        # self.btn_schedule.grid(row = 0, column = 6, padx = 10, pady = 10)
+        self.lbl_topic.place(x = 26, y = 20)
+        self.lbl_description.place(x = 200, y = 30)
+        self.btn_schedule.place(x = 780, y = 10)
+        
         
         # row1
         self.lbl_r1c1.grid(row = 1, column = 1, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
         self.lbl_r1c2.grid(row = 1, column = 2, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
         self.lbl_r1c3.grid(row = 1, column = 3, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
         self.lbl_r1c4.grid(row = 1, column = 4, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
-        
+        # self.lbl_r1c1.place(x = 80, y = 120)
+        # self.lbl_r1c2.place(x = 185, y = 120)
+        # self.lbl_r1c3.place(x = 290, y = 120)
+        # self.lbl_r1c4.place(x = 395, y = 120)
+                       
         # row2
         self.lbl_r2c1.grid(row = 2, column = 1, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
         self.lbl_r2c2.grid(row = 2, column = 2, sticky = tk.NW + tk.SE, padx = 1, pady = 1)
@@ -105,8 +115,8 @@ class Window(tk.Frame):
         # row7
         self.lbl_fixcost.grid(row = 7, column = 1, columnspan = 2, sticky = tk.NW + tk.SE, padx = 1, pady = 15)
         self.lbl_cost.grid(row = 7, column = 4, columnspan = 2, sticky = tk.NW + tk.SE, padx = 1, pady = 15)
-        self.btn_order.grid(row = 7, column = 6, padx = 1, pady = 15)
-        
+        # self.btn_order.grid(row = 7, column = 6, padx = 1, pady = 15)
+        self.btn_order.place(x = 780, y = 480)
         
     
     def clickBtnSchedule(self):
@@ -117,14 +127,11 @@ class Window(tk.Frame):
 
 
 
-
-first_page = Window()
-first_page.master.title("Restaurant Game")
-# first_page.master.geometry('1000x600')
+ordering_page = Ordering()
+ordering_page.master.title("餐廳經營小遊戲")
+ordering_page.master.geometry("900x600+200+30")
 # first_page.master.configure(background = 'Lavender')
  # 粉紫 LavenderBlush # 粉橘 OldLace # 薄荷奶油 MintCream # 淡鵝黃 LightYellow # 淡黃橘LemonChiffon
 
-# header_label = tk.Label(window, text = '餐廳經營管理遊戲')
-# header_label.pack()
+ordering_page.mainloop()
 
-first_page.mainloop()
