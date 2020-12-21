f9a821aae80db172731298bc5e81a8369a08e0a4
r09741021 <75658521+r09741021@users.noreply.github.com>
2020-12-21 12:16:51 +0800

    更新PPT4&5
    更新物件位置

--- PPT4-day1.py
+++ PPT4-day1.py
@@ -15,31 +15,32 @@ def clickBtnCalendar(self):
 know_content = "小知識11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
 
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("900x600+200+30")
 
 # canvas =tk.Canvas(root, width =800,height = 600, bg="#fff")
 # canvas.place(x = 0, y = 0)
 
 # 字體設定
-f1 = tkFont.Font(size = 30, family = "微軟正黑體", weight="bold")
+f1 = tkFont.Font(size = 30, family = "微軟正黑體")
 f2 = tkFont.Font(size = 14, family = "微軟正黑體") 
+f3 = tkFont.Font(size = 12, family = "微軟正黑體") 
 
 # 元件內容
-lbl_day = tk.Label(root, text = "Day1", font = f1) 
+lbl_day = tk.Label(root, text = "Day 1", font = f1) 
 lbl_know = tk.Label(root, text = know_content, font = f2, borderwidth = 2, relief = "ridge", wraplength = 300, justify = 'left')
-btn_optA = tk.Button(root, text = "OptA", command = clickBtnOptA, width = 10,height = 2, font = f2, bg = 'Lavender')
-btn_optB = tk.Button(root, text = "OptB", command = clickBtnOptB, width = 10,height = 2, font = f2, bg = 'Lavender')
-btn_calendar = tk.Button(root, text = "行事曆", command = clickBtnCalendar, width = 10,height = 2, font = f2, bg = 'Lavender')
+btn_optA = tk.Button(root, text = "OptA", command = clickBtnOptA, width = 7,height = 2, font = f2, bg = 'Lavender')
+btn_optB = tk.Button(root, text = "OptB", command = clickBtnOptB, width = 7,height = 2, font = f2, bg = 'Lavender')
+btn_calendar = tk.Button(root, text = "行事曆", command = clickBtnCalendar, width = 7,height = 2, font = f2, bg = 'Lavender')
         
 # 元件位置
-lbl_day.place(relx= 0.08, rely=0.15, width=100, height=80)
+lbl_day.place(x= 26, y=20, width=100, height=80)
 lbl_know.place(relx= 0.2, rely=0.35, width=350, height=200)
 btn_optA.place(relx= 0.2, rely=0.75)
 btn_optB.place(relx= 0.5, rely=0.75)
-btn_calendar.place(relx= 0.8, rely=0.1)
+btn_calendar.place(x= 780, y=20)
 
 # 遊戲表頭
-root.title("Restaurant Game")
+root.title("餐廳經營小遊戲")
 
 root.mainloop()
 
--- PPT5-行事曆 .py
+++ PPT5-行事曆 .py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.font as tkFont
-import pandas as pd
-import texttable as tb
+from tkinter import ttk
+
 
 # 點擊按鈕後指令
 def clickBtnReturn(self):
@@ -10,7 +10,7 @@ def clickBtnReturn(self):
 admin_content = "經營背景11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
 
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("900x600+200+30")
 
 # canvas =tk.Canvas(root, width =800,height = 600, bg="#fff")
 # canvas.place(x = 0, y = 0)
@@ -18,59 +18,66 @@ root.geometry("800x600")
 days = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"]  
 events = [1, 2, 3, 4, 5, 6, 7]
 
-# dict1 = {"日期": days, "活動": event}
-# calendar = pd.DataFrame(dict1)
+# 行事曆表格
 
-# 做行事曆表格時遇到困難，以下是用matplotlib.pyplot做的表格會跳出新視窗
-"""
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+tree_item=ttk.Treeview(root, selectmode="extended", columns=("Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"))#表格
+style = ttk.Style()
+style.configure(tree_item, font=("微軟正黑體", 14))
 
-fig, ax = plt.subplots()
+# tree_item.pack(expand=YES, fill=BOTH)
+tree_item["columns"]=("Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7")
+tree_item.column("#0",minwidth=0,width=70, anchor='center')
+tree_item.column("Day 1",minwidth=0,width=100, anchor='center')   #表示列,不顯示
+tree_item.column("Day 2",minwidth=0,width=100, anchor='center')
+tree_item.column("Day 3",minwidth=0,width=100, anchor='center')
+tree_item.column("Day 4",minwidth=0,width=100, anchor='center')
+tree_item.column("Day 5",minwidth=0,width=100, anchor='center')
+tree_item.column("Day 6",minwidth=0,width=100, anchor='center')
+tree_item.column("Day 7",minwidth=0,width=100, anchor='center')
 
-days = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"]  
-events = [1, 2, 3, 4, 5, 6, 7]
-table_vals = []
-table_vals.append(days)
-table_vals.append(events)
+tree_item.heading("#0",text="Day")
+tree_item.heading("Day 1",text="Day 1")  #顯示表頭
+tree_item.heading("Day 2",text="Day 2")
+tree_item.heading("Day 3",text="Day 3")
+tree_item.heading("Day 4",text="Day 4")
+tree_item.heading("Day 5",text="Day 5")
+tree_item.heading("Day 6",text="Day 6")
+tree_item.heading("Day 7",text="Day 7")
 
-# hide axes
-fig.patch.set_visible(False)
-ax.axis('off')
-ax.axis('tight')
+# tree.heading("#0", text="C/C++ compiler")
+# tree.column("#0", minwidth=0, width=100, stretch=NO)
 
 
-df = pd.DataFrame(table_vals)
-ax.table(cellText=df.values, loc='center')
-fig.tight_layout()
-plt.show()
-"""
+tree_item.insert("",1,text="活動", values=events)#插入資料
+tree_item.place(x=50, y=140, height=120)
+
+
 
 
 # 字體設定
-f1 = tkFont.Font(size = 30, family = "微軟正黑體", weight="bold")
+f1 = tkFont.Font(size = 30, family = "微軟正黑體")
 f2 = tkFont.Font(size = 14, family = "微軟正黑體") 
+f3 = tkFont.Font(size = 12, family = "微軟正黑體") 
 
 # 元件內容
 lbl_calendar = tk.Label(root, text = "行事曆", font = f1) 
 lbl_admin = tk.Label(root, text = admin_content, font = f2, borderwidth = 2, relief = "ridge", wraplength = 300, justify = 'left')
-btn_return = tk.Button(root, text = "返回", command = clickBtnReturn, width = 10,height = 2, font = f2, bg = 'Lavender')
+btn_return = tk.Button(root, text = "返回", command = clickBtnReturn, width = 7,height = 2, font = f2, bg = 'Lavender')
 
-lbl_days = tk.Label(root, text = days, font = f2, borderwidth = 2, relief = "ridge", wraplength = 650)
-lbl_events = tk.Label(root, text = events, font = f2, borderwidth = 2, relief = "ridge", wraplength = 650)
+# lbl_days = tk.Label(root, text = days, font = f2, borderwidth = 2, relief = "ridge", wraplength = 650)
+# lbl_events = tk.Label(root, text = events, font = f2, borderwidth = 2, relief = "ridge", wraplength = 650)
 
         
 # 元件位置
 lbl_calendar.place(relx= 0.4, rely=0.1, width=120, height=80)
 lbl_admin.place(relx= 0.25, rely=0.5, width=350, height=200)
-btn_return.place(relx= 0.8, rely=0.8)
+btn_return.place(x= 780, y=480)
 
-lbl_days.place(relx= 0.1, rely=0.25, width=700, height=30)
-lbl_events.place(relx= 0.1, rely=0.3, width=700, height=80)
+# lbl_days.place(relx= 0.1, rely=0.25, width=700, height=30)
+# lbl_events.place(relx= 0.1, rely=0.3, width=700, height=80)
 
 # 遊戲表頭
-root.title("Restaurant Game")
+root.title("餐廳經營小遊戲")
 
 root.mainloop()
 
